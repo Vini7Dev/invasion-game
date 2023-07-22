078c77682b3ca8b3af24974fd780e3c1d78f0037
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-07-22 18:49:33 -0300

    feat: :art: camera follow the player

---
+++ InvasionGame/Assets/Scripts/Camera/MainCameraController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour
+{
+    public float cameraTopDistance = 25;
+    public float cameraFollowSpeed = 5;
+
+    Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 positonToFollow = new Vector3(
+            player.position.x,
+            cameraTopDistance,
+            player.position.z
+        );
+
+        transform.position = Vector3.Lerp(
+            transform.position,
+            positonToFollow,
+            Time.deltaTime * cameraFollowSpeed
+        );
+    }
+}
