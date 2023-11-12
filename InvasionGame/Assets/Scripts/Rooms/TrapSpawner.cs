d34b87abc4f0db8ac2a3085732eb16c970bbbb63
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-11-12 13:46:29 -0300

    feat: :art: create trap spawner

---
+++ InvasionGame/Assets/Scripts/Rooms/TrapSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag != "RoomFloor") return;
+
+        if (objectToSpawn)
+        {
+            GameObject trapSpawned = Instantiate(
+                objectToSpawn,
+                transform.position,
+                objectToSpawn.transform.rotation
+            );
+
+            trapSpawned.transform.parent = transform.parent;
+        }
+
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
