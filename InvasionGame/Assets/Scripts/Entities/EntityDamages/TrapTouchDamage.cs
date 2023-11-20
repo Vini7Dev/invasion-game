d40e322692a8d9d98b6a0cb84259113855cbe6fc
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-11-20 11:59:55 -0300

    feat: :art: update spikes model

--- InvasionGame/Assets/Scripts/Entities/EntityDamages/TrapTouchDamage.cs
+++ InvasionGame/Assets/Scripts/Entities/EntityDamages/TrapTouchDamage.cs
@@ -4,8 +4,36 @@ using UnityEngine;
 
 public class TrapTouchDamage : DealTouchDamage
 {
+    public GameObject trapDamageObject;
+    public float timeToActiveTrap = 1;
+
+    bool isActive, inTimer;
+
+    void Update()
+    {
+        if (!inTimer) {
+            StartCoroutine(ToggleTrapAtiveTimer());
+        }
+    }
+
+    IEnumerator ToggleTrapAtiveTimer()
+    {
+        inTimer = true;
+
+        yield return new WaitForSeconds(timeToActiveTrap);
+
+        isActive = !isActive;
+        inTimer = false;
+
+        if (trapDamageObject) {
+            trapDamageObject.SetActive(isActive);
+        }
+    }
+
     protected override void ApplyDamage(Collider other)
     {
+        if (!isActive) return;
+
         EntitySkills entitySkills = other.GetComponent<EntitySkills>();
 
         if (entitySkills != null && entitySkills.canWalkInTrap) return;
